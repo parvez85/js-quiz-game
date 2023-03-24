b3146598b590b3aa7e5a576debd8e02c06888dbd
AP <a.parvez85@gmail.com>
2023-03-24 19:45:51 +0000

    stater files

---
+++ assets/js/logic.js
@@ -0,0 +1,27 @@
+// javascript for the app
+var question = [
+  {question: "What is HTML?",
+    choice1: "Hyper Text Markup Language",
+    choice2: "Hyper Text Makup Language",
+    choice3: "Hyper Text Makeup Language",
+    choice4: "Hyper Text Markup Language",
+    
+    answer: "Hyper Text Markup Language"
+    
+},
+{
+    question: "What is CSS?",
+    choice1: "Cascading Style Sheets",
+    choice2: "Cascading Style Sheets",
+    choice3: "Cascading Style Sheets",
+    choice4: "Cascading Style Sheets",
+    
+    answer: "Cascading Style Sheets"
+ }
+] 
+
+console.log(question)
+
+var questionIndex = 0
+
+function questionBox() 
